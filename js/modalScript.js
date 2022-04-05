daf807e5d0ca4819b61a9533d2005574868460c8
fizjo <sadfg@sdfd.pl>
2022-04-05 23:18:48 +0200

    ready project with change colors

--- js/modalScript.js
+++ js/modalScript.js
@@ -3,16 +3,14 @@ const modalShadow = document.querySelector('.modal-shadow');
 const closeModalBtn = document.querySelector('.close');
 
 
-const showModal= () => {
-  if(!(modalShadow.style.display === 'block')){
+const showModal = () => {
+  if (!(modalShadow.style.display === 'block')) {
     modalShadow.style.display = 'block';
   } else {
     modalShadow.style.display = 'none'
-  };
-
+  }
   modalShadow.classList.toggle('modal-animation');
 }
-
 
 
 infoBtn.addEventListener('click', showModal);
